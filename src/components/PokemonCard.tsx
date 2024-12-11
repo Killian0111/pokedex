f7ffad68d73e54285c568eae9da9cc451447fb1e
Killian0111 <45.killian.georges@gmail.com>
2024-12-11 12:25:40 +0100

    Pokemon Arceus

---
+++ src/components/PokemonCard.tsx
@@ -0,0 +1,13 @@
+function PokemonCard() {
+  return (
+    <figure>
+      <img
+        src="https://www.pokepedia.fr/images/thumb/2/26/Arceus-DEPS.png/250px-Arceus-DEPS.png"
+        alt="Pokemon_Arceus"
+      />
+      <figcaption>Arceus</figcaption>
+    </figure>
+  );
+}
+
+export default PokemonCard;
