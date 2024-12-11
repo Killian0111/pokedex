76a587b6957fe1d0552dd9d71b5160eb9cc16865
Killian0111 <45.killian.georges@gmail.com>
2024-12-11 16:42:49 +0100

    React 4

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,21 @@ import PokemonCard from "./components/PokemonCard";
 function App() {
   return (
     <div>
-      <PokemonCard />
+      <PokemonCard pokemon={pokemonList[1]} />
     </div>
   );
 }
 
 export default App;
+
+const pokemonList = [
+  {
+    name: "Pokemon_Arceus",
+    imgSrc:
+      "https://www.pokepedia.fr/images/thumb/2/26/Arceus-DEPS.png/250px-Arceus-DEPS.png",
+  },
+  {
+    name: "mew",
+    imgSrc: null,
+  },
+];
--- src/components/PokemonCard.tsx
+++ src/components/PokemonCard.tsx
@@ -1,17 +1,4 @@
-const pokemonList = [
-  {
-    name: "Pokemon_Arceus",
-    imgSrc:
-      "https://www.pokepedia.fr/images/thumb/2/26/Arceus-DEPS.png/250px-Arceus-DEPS.png",
-  },
-  {
-    name: "mew",
-  },
-];
-
-function PokemonCard() {
-  const pokemon = pokemonList[1];
-
+function PokemonCard({ pokemon }) {
   return (
     <figure>
       <h2>{pokemon.name}</h2>
