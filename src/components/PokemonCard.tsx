ac5f5565168eabce32ea268a76124cb90dbbb562
Killian0111 <45.killian.georges@gmail.com>
2024-12-11 15:47:40 +0100

    React 3 mew

--- src/components/PokemonCard.tsx
+++ src/components/PokemonCard.tsx
@@ -1,11 +1,25 @@
+const pokemonList = [
+  {
+    name: "Pokemon_Arceus",
+    imgSrc:
+      "https://www.pokepedia.fr/images/thumb/2/26/Arceus-DEPS.png/250px-Arceus-DEPS.png",
+  },
+  {
+    name: "mew",
+  },
+];
+
 function PokemonCard() {
+  const pokemon = pokemonList[1];
+
   return (
     <figure>
-      <img
-        src="https://www.pokepedia.fr/images/thumb/2/26/Arceus-DEPS.png/250px-Arceus-DEPS.png"
-        alt="Pokemon_Arceus"
-      />
-      <figcaption>Arceus</figcaption>
+      <h2>{pokemon.name}</h2>
+      {pokemon.imgSrc ? (
+        <img src={pokemon.imgSrc} alt={pokemon.name} />
+      ) : (
+        <p>???</p>
+      )}
     </figure>
   );
 }
